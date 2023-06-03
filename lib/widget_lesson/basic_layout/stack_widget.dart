3793adb178e462b4cb992337f08c5965bb465a5c
Zainal Salamun <zainalrtf@gmail.com>
2023-06-03 21:33:49 +0700

    stack widget

---
+++ lib/widget_lesson/basic_layout/stack_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StackWidget extends StatelessWidget {
+  const StackWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("FIC - Stack"),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: const [
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 50,
+                  ),
+                  Positioned(
+                    top: -4.0,
+                    right: -4.0,
+                    child: CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Colors.red,
+                      child: Text(
+                        "3",
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ])));
+  }
+}
